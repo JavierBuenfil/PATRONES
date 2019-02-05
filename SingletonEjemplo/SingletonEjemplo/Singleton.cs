bcdfbe1bd60af7efd2c83657ec70d958401edb05
JavierBuenfil <javierbuenfil95@hotmail.com>
2019-02-05 10:36:50 -0600

    Segundo Commit Ejemplosingleton 05/02/19

---
+++ SingletonEjemplo/SingletonEjemplo/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SingletonEjemplo
+{
+    public partial class Singleton : Form
+    {
+        private static Singleton instance = null;
+        public Singleton()
+        {
+            InitializeComponent();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        private void Singleton_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
